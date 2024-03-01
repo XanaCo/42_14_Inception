16751ca03ee291c8ea37ba9fdc346824de0db1fd
AC <ancolmen@student.42.fr>
2024-03-01 17:24:59 +0100

    Mandatory OK

--- srcs/requirements/mariadb/tools/scriptMDB.sh
+++ srcs/requirements/mariadb/tools/scriptMDB.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-service mariadb start
+if [ -e "/var/lib/mysql/config-done.log" ]
+then
+    echo "Database already set\n"
+else
+    service mariadb start
 
-sleep 5
+    sleep 5
 
-mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" \
-&& mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';" \
-&& mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';" \
-&& mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASS}';" \
-&& mysql -e "FLUSH PRIVILEGES;" \
+    mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+    mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';"
+    mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';"
+    mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASS}';"
+    mysql -e "FLUSH PRIVILEGES;"
 
-sleep 1
+    sleep 1
 
-mysqladmin --user=root --password=${MYSQL_ROOT_PASS} shutdown
+    mysqladmin --user=root --password=${MYSQL_ROOT_PASS} shutdown
+
+    touch /var/lib/mysql/config-done.log
+fi
 
 exec mysqld_safe
